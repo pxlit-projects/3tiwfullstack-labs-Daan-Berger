158ceb28382094973f7635d30e6b80fcb1f30e55
daanberger <daan.berger@telenet.be>
2025-10-15 10:50:09 +0200

    Add employee creation and retrieval endpoints to EmployeeController

--- backend-java/SpringCloudPxl/employee-service/src/main/java/be/pxl/services/controller/EmployeeController.java
+++ backend-java/SpringCloudPxl/employee-service/src/main/java/be/pxl/services/controller/EmployeeController.java
@@ -1,24 +1,46 @@
 package be.pxl.services.controller;
 
+import be.pxl.services.controller.dto.EmployeeRequest;
+import be.pxl.services.controller.dto.EmployeeResponse;
 import be.pxl.services.services.IEmployeeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/employee")
 @RequiredArgsConstructor
 public class EmployeeController {
 
-
     private final IEmployeeService employeeService;
 
+    @PostMapping
+    public ResponseEntity<Void> addEmployee(@Valid @RequestBody EmployeeRequest employeeRequest) {
+        employeeService.addEmployee(employeeRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable long id) {
+        return new ResponseEntity<>(employeeService.getEmployeeById(id), HttpStatus.OK);
+    }
 
     @GetMapping
-    public ResponseEntity getEmployees() {
-        return new ResponseEntity(employeeService.getAllEmployees(), HttpStatus.OK);
+    public ResponseEntity<List<EmployeeResponse>> getAllEmployees(){
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
     }
+
+    @GetMapping("/department/{departmentId}")
+    public ResponseEntity<List<EmployeeResponse>> getEmployeeByDepartmentId(@PathVariable long departmentId) {
+        return new ResponseEntity<>(employeeService.getEmployeeByDepartmentId(), HttpStatus.OK);
+    }
+    @GetMapping("/organization/{organizationId}")
+    public ResponseEntity<List<EmployeeResponse>> getEmployeeByOrganizationId(@PathVariable long organizationId) {
+        return new ResponseEntity<>(employeeService.getEmployeeByOrganizationId(), HttpStatus.OK);
+    }
+
 }
