988920a62bf12877eba1a0bf72deb76f606805ca
daanberger <daan.berger@telenet.be>
2025-10-15 10:47:12 +0200

    Update IEmployeeService interface to include employee request and response DTOs

--- backend-java/SpringCloudPxl/employee-service/src/main/java/be/pxl/services/services/IEmployeeService.java
+++ backend-java/SpringCloudPxl/employee-service/src/main/java/be/pxl/services/services/IEmployeeService.java
@@ -1,10 +1,18 @@
 package be.pxl.services.services;
 
-import be.pxl.services.domain.Employee;
+import be.pxl.services.controller.dto.EmployeeRequest;
+import be.pxl.services.controller.dto.EmployeeResponse;
 
 import java.util.List;
 
 public interface IEmployeeService {
-    List<Employee> getAllEmployees();
+    List<EmployeeResponse> getAllEmployees();
 
+    void addEmployee(EmployeeRequest employeeRequest);
+
+    EmployeeResponse getEmployeeById(Long id);
+
+    List<EmployeeResponse> getEmployeeByDepartmentId();
+
+    List<EmployeeResponse> getEmployeeByOrganizationId();
 }
